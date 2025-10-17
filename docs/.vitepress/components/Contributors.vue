c1be1ad893b1a1f7b5b83b2dd162b32d5ef63859
Ragnar Lothbrok <ragnarlothbrokjr@proton.me>
2025-10-17 18:54:03 +0100

    ✨ (Contributors.vue): Add Contributors component to display a list of project contributors fetched from GitHub API and display their avatars.

---
+++ docs/.vitepress/components/Contributors.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+
+const contributors = ref([]);
+
+onMounted(async () => {
+  try {
+    const response = await fetch("https://api.github.com/repos/RagnarLothbrok-Odin/StremioGuide/contributors");
+    if (response.ok) {
+      const data = await response.json();
+      contributors.value = data.filter(contributor => contributor.login !== 'actions-user').map((contributor) => ({
+        name: contributor.login,
+        avatar: contributor.avatar_url,
+      }));
+    }
+  } catch {
+    /* ... */
+  }
+});
+</script>
+
+<template>
+  <div class="contributors-container">
+    <h2>Contributors</h2>
+    <div class="users">
+      <a v-for="{ name, avatar } of contributors" :key="name" :href="`https://github.com/${name}`" target="_blank" rel="noopener noreferrer" :aria-label="`${name} on GitHub`" :data-tooltip="name" data-position="top" class="tooltip">
+        <img class="user" loading="lazy" :src="avatar" width="50" height="50" :alt="`${name}'s avatar`" />
+      </a>
+    </div>
+  </div>
+</template>
