22afd04d19160c0131d66cd35aac9f161f6dec2c
Ragnar Lothbrok <ragnarlothbrokjr@proton.me>
2025-10-17 18:06:34 +0100

    📝 (config.ts): add editLink configuration for editing pages on GitHub.

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -43,6 +43,11 @@ export default defineConfig({
 
     outline: [2, 3],
 
+    editLink: {
+      pattern: "https://github.com/RagnarLothbrok-Odin/StremioGuide/edit/main/docs/:path",
+      text: "Edit this page on GitHub",
+    },
+
     nav: [
       { text: 'Home', link: '/' },
     ],
